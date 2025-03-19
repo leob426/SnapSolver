7fd69f5fe27e150ab6f3cf4f6e413b106c9a4920
Leo <Leo.berezowsky15@gmail.com>
2025-03-19 09:24:07 -0700

    Auto-release version 1.0.10

--- SnapSolver.py
+++ SnapSolver.py
@@ -16,8 +16,8 @@ import logging
 ########################
 # Self-update constants/variables
 ########################
-CURRENT_VERSION = "1.0.11"  # Update each time you release
-UPDATE_VERSION_URL = "https://raw.githubusercontent.com/leob426/SnapSolver/main/latest_version.txt"
+CURRENT_VERSION = "1.0.10"  # Update each time you release
+UPDATE_VERSION_URL = "https://github.com/leob426/SnapSolver/blob/main/latest_version.txt"
 UPDATE_EXE_URL = "https://github.com/leob426/SnapSolver/releases/latest/download/SnapSolver_latest.exe"
 EXE_NAME = "SnapSolver.exe"
 NEW_EXE_NAME = "SnapSolver_new.exe"
@@ -569,6 +569,7 @@ if __name__ == "__main__":
 
     threading.Thread(target=main_loop, args=(root,), daemon=True).start()
     root.mainloop()
+
 
 
 
